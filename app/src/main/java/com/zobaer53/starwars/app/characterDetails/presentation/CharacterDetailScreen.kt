9554746d79b8b773b5e80d5a317b2f1bdad5ca25
zobaer53 <hossain.zobaer11858@gmail.com>
2023-11-23 19:56:14 +0600

    refactor-- character details ui

--- app/src/main/java/com/zobaer53/starwars/app/characterDetails/presentation/CharacterDetailScreen.kt
+++ app/src/main/java/com/zobaer53/starwars/app/characterDetails/presentation/CharacterDetailScreen.kt
@@ -4,14 +4,18 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -31,7 +35,8 @@ fun CharacterDetailScreen(
     characterId: String,
     viewModel: CharacterViewModel = hiltViewModel(navController.getBackStackEntry(AppScreen.MainScreen.route))
 ) {
-    Scaffold(topBar = { TopAppBar(title = { Text(text = "Details Screen") }) }) {
+    Scaffold(topBar = { TopAppBar(title = { Text(text = "Details Screen", color = Color.Black) },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor =MaterialTheme.colorScheme.primary)) }) {
 
         Box(modifier = Modifier.padding( 16.dp)) {
             val characterPagingItems: LazyPagingItems<Character> =
--- app/src/main/java/com/zobaer53/starwars/app/characterDetails/presentation/component/CharacterDetailsItem.kt
+++ app/src/main/java/com/zobaer53/starwars/app/characterDetails/presentation/component/CharacterDetailsItem.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,7 +22,7 @@ fun CharacterDetailsItem(
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 50.dp),
+            .padding(top = 60.dp),
         elevation = CardDefaults.cardElevation(8.dp)
     ) {
         Column(
